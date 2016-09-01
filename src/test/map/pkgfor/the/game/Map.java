f79f5f9262a20025a9264c537e16fc8af7755334
glib2002 <glebix@ukr.net>
2016-09-01 14:32:59 +0300

    Update Map.java

--- src/test/map/pkgfor/the/game/Map.java
+++ src/test/map/pkgfor/the/game/Map.java
@@ -27,9 +27,9 @@ public class Map extends javax.swing.JFrame {
         tile.setSize(16, 16);
         tile.setPath("C:\\Users\\Глеб\\workspace\\Tank butttle\\src\\game\\tank\\assets\\ground2.png");
         tile.setImage(tile.getPath());
-        jPanel1.add(tile);
-        jPanel2.add(tile);
-        tile.repaint();
+        
+        
+        
         //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
         tile2 = new Tile();
         tile2.setLocation(22,0);
@@ -37,7 +37,11 @@ public class Map extends javax.swing.JFrame {
         tile2.setPath("C:\\Users\\Глеб\\workspace\\Tank butttle\\src\\game\\tank\\assets\\ground1.png");
         tile2.setImage(tile2.getPath());
         
+        
+        jPanel1.add(tile);
+        jPanel1.add(tile2);
         tile2.repaint();
+        tile.repaint();
         
     }
 
@@ -68,16 +72,7 @@ public class Map extends javax.swing.JFrame {
             .addGap(0, 16, Short.MAX_VALUE)
         );
 
-        javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
-        jPanel2.setLayout(jPanel2Layout);
-        jPanel2Layout.setHorizontalGroup(
-            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 16, Short.MAX_VALUE)
-        );
-        jPanel2Layout.setVerticalGroup(
-            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 0, Short.MAX_VALUE)
-        );
+        
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -86,14 +81,13 @@ public class Map extends javax.swing.JFrame {
             .addGroup(layout.createSequentialGroup()
                 .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(0, 362, Short.MAX_VALUE))
+                
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
-                    .addComponent(jPanel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    
                     .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
                 .addGap(0, 284, Short.MAX_VALUE))
         );
@@ -142,6 +136,6 @@ public class Map extends javax.swing.JFrame {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JPanel jPanel1;
-    private javax.swing.JPanel jPanel2;
+    
     // End of variables declaration//GEN-END:variables
 }
